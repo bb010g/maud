b980058284c9493cf830b03dd1ed74e56cd339ad
Chris Wong <lambda.fairy@gmail.com>
2017-05-13 14:17:58 +1200

    Update documentation

---
+++ maud_extras/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Charset","Generate a `<meta charset=\"...\">` element."],["Css","Links to an external stylesheet."],["Js","Links to an external script."],["Meta","Generate a `<meta>` element."],["MetaProperty","Generate a `<meta property>` element."],["Title","Generate a `<title>` element."]]});
